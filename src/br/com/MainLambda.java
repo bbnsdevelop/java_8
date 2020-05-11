f35ea569e5da5d193b05350e2ff34500c6b67f29
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-10 21:09:49 -0300

    chamando uma interface funcional da classe Runnable com lambda

---
+++ src/br/com/MainLambda.java
@@ -0,0 +1,20 @@
+package br.com;
+
+public class MainLambda {
+	
+	public static void main(String[] args) {
+		
+		Runnable run = new Runnable() {			
+			@Override
+			public void run() {
+				System.out.println("Forma antiga");				
+			}
+		};
+		run.run();
+		
+		Runnable run2 = () -> System.out.println("invocando uma interface funcional");
+		run2.run();
+		
+	}
+
+}
