21ba2a63d8de049b7f529da092f1059c216887f2
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-10 21:19:24 -0300

    criando uma interface funcional e chamando o metodo

---
+++ src/br/com/MainInterfaceFunctional.java
@@ -0,0 +1,20 @@
+package br.com;
+
+public class MainInterfaceFunctional {
+
+	public static void main(String[] args) {
+		
+		Num n1 = () -> 333.11;
+	
+		System.out.println(n1.getValor());
+		
+		Num n = () -> {
+				double num = 333.11 + 277.99;
+				return num;
+			};
+		
+		System.out.println(n.getValor());
+		
+	}
+
+}
