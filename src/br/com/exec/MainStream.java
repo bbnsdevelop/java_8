440b20d4bc7a416ba5ee8cda2202262417565861
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-12 21:24:35 -0300

    alteração do nome da classe

--- src/br/com/exec/MainStream.java
+++ src/br/com/exec/MainStream.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class Main {
+public class MainStream {
 
 	public static void main(String[] args) {		
 		
